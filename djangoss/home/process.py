0473a4c9f8fbc53ca8defea86dfc932b45d09188
nguyenquyet27 <ndquyet27@gmail.com>
2019-06-06 00:36:18 +0700

    process

--- djangoss/home/process.py
+++ djangoss/home/process.py
@@ -6,72 +6,72 @@ import tokenizing
 import weight
 from collections import Counter
 
-
-
-data_path = general.check_data_path("datas.json")
-
-dataset = open(data_path)
-data = [eval(i) for i in dataset]
-ProductName = list()
-for i in data:
-    ProductName.append(i["ProductName"])
-corpus = list()
-for name in ProductName:
-    terms = tokenizing.get_terms(name,lemmatize=False, stemming=False)
-    bag_of_words = Counter(terms)
-    corpus.append(bag_of_words)
-
-#input
 x = input("Nhap cau truy van: ")
 
-x_ = tokenizing.get_terms(x,lemmatize=False, stemming=False)
-bag = Counter(x_)
-corpus.append(bag)
+def process(x):
 
-#compute tf-idf
-tf = weight.compute_tf(corpus)
-idf = weight.compute_idf(corpus)
+    data_path = general.check_data_path("datas.json")
 
-represent_tfidf = weight.compute_weight(tf, idf)
+    dataset = open(data_path)
+    data = [eval(i) for i in dataset]
+    ProductName = list()
+    for i in data:
+        ProductName.append(i["ProductName"])
+    corpus = list()
+    for name in ProductName:
+        terms = tokenizing.get_terms(name,lemmatize=False, stemming=False)
+        bag_of_words = Counter(terms)
+        corpus.append(bag_of_words)
 
-#compute cosi
-cosi = list()
-k = len(represent_tfidf) - 1
-b = np.array(represent_tfidf[k])
-normb = np.linalg.norm(b)
-for i in range(0,k):
-    a = np.array(represent_tfidf[i])
-    dot = np.dot(a, b)
-    norma = np.linalg.norm(a)
-    cos = dot / (norma * normb)
-    cosi.append(cos)
+    #xu li input
+    x_ = tokenizing.get_terms(x,lemmatize=False, stemming=False)
+    bag = Counter(x_)
+    corpus.append(bag)
 
-#output
-z = copy.deepcopy(cosi)
-z_ = z.sort(reverse = True)
-na = set()
-for i in range(0,10):
-	for j in range(0,len(cosi)):
-		if (z[i] == cosi[j]):
-			na.add(j)
+    #compute tf-idf
+    tf = weight.compute_tf(corpus)
+    idf = weight.compute_idf(corpus)
 
-hi = list(na)
-Price = list()  
-for i in data:
-    Price.append(i["Price"])
+    represent_tfidf = weight.compute_weight(tf, idf)
 
-Company = list() 
-for i in data:
-    Company.append(i["Company"])
+    #compute cosi
+    cosi = list()
+    k = len(represent_tfidf) - 1
+    b = np.array(represent_tfidf[k])
+    normb = np.linalg.norm(b)
+    for i in range(0,k):
+        a = np.array(represent_tfidf[i])
+        dot = np.dot(a, b)
+        norma = np.linalg.norm(a)
+        cos = dot / (norma * normb)
+        cosi.append(cos)
 
-Distributor = list()
-for i in data:
-    Distributor.append(i["Distributor"])
+    #output
+    z = copy.deepcopy(cosi)
+    z_ = z.sort(reverse = True)
+    na = set()
+    for i in range(0,10):
+            for j in range(0,len(cosi)):
+                    if (z[i] == cosi[j]):
+                            na.add(j)
 
-for i in hi:
-    print (ProductName[i])
-    print (Price[i])
-    print (Company[i])
-    print (Distributor[i])
+    hi = list(na)
+    Price = list()  
+    for i in data:
+        Price.append(i["Price"])
+
+    Company = list() 
+    for i in data:
+        Company.append(i["Company"])
+
+    Distributor = list()
+    for i in data:
+        Distributor.append(i["Distributor"])
+
+    for i in hi:
+        print (ProductName[i])
+        print (Price[i])
+        print (Company[i])
+        print (Distributor[i])
     
-    
+process(x)
