d26b69bf9458ded5182de7f03cdefa6ef88753b6
tks1998 <tks1998>
2019-06-06 00:39:34 +0700

    make method post get input from user

--- djangoss/home/urls.py
+++ djangoss/home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:id>/', views.page2)
+    path('<int:id>/', views.page2),
+    path('detail', views.process , name = "search")
 ]
--- djangoss/home/views.py
+++ djangoss/home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 def index(request):
-    var ={}
+    
+    var = {
+        'sanpham' : 'anc'
+    }
     return render(request, 'pages/pages1.html',var)
 def page2(request, id):
     return render(request, 'pages/pages2.html')
+def process(request):
+    x = request.POST["chuoi"]
+    return HttpResponse(x)
